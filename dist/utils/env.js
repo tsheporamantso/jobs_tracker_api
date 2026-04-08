1e75871488811bbbcc5caa7de5bdd764a3d53dd1
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-08 10:16:11 +0200

    Add env helper function to assert key

---
+++ dist/utils/env.js
@@ -0,0 +1,8 @@
+"use strict";
+const getEnvVariable = (key) => {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Missing environment variable ${key}`);
+    }
+    return value;
+};
---
+++ src/utils/env.ts
@@ -0,0 +1,8 @@
+const getEnvVariable = (key: string): string => {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Missing environment variable ${key}`);
+  }
+  return value;
+};
