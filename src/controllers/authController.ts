35e83ab4a7430063c3139204e8c6ccd5382dffec
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-09 12:39:08 +0200

    feat(auth): create user and hashed user password with bcryptjs

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,24 @@
+import User from "../models/User";
 import { StatusCodes } from "http-status-codes";
 import { asyncWrapper } from "../middleware/async";
 import { AsyncController } from "../types/asyncController";
+import bcrypt from "bcryptjs";
 
 export const register: AsyncController = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.OK).json({
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  const user = await User.create({ ...tempUser });
+  console.log(user);
+
+  res.status(StatusCodes.CREATED).json({
     success: true,
     msg: "User registered in successfully",
+    user,
   });
 });
 
