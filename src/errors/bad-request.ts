9c181c19e4a275456549c046607e90401a0e281b
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-09 10:21:30 +0200

    refactor(errors): change bad request class name to BadRequestError

--- src/errors/bad-request.ts
+++ src/errors/bad-request.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { CustomErrorAPI } from "./custom-error";
 
-export class BadRequest extends CustomErrorAPI {
+export class BadRequestError extends CustomErrorAPI {
   constructor(message: string, statusCode: number = StatusCodes.BAD_REQUEST) {
     super(message, statusCode);
   }
