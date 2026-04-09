279df07344348c05240ef80de9e35e8bfe426225
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-09 14:27:33 +0200

    feat(models): add user login functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,8 @@ import User from "../models/User";
 import { StatusCodes } from "http-status-codes";
 import { asyncWrapper } from "../middleware/async";
 import { AsyncController } from "../types/asyncController";
+import { BadRequestError } from "../errors/bad-request";
+import { UnauthenticatedError } from "../errors/unauthenticated";
 
 export const register: AsyncController = asyncWrapper(async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -16,8 +18,22 @@ export const register: AsyncController = asyncWrapper(async (req, res) => {
 });
 
 export const login: AsyncController = asyncWrapper(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const token = user.createJWT();
+
   res.status(StatusCodes.OK).json({
     success: true,
     msg: "User logged in successfully",
+    user: { name: user.name },
+    token,
   });
 });
